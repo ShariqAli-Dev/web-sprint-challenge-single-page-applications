2dc3d79fee4b8b0c8ea5ae49d5fffcc2033692be
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-17 20:54:06 -0400

    Completed links and created component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import { Link, Route, Switch } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header class="store-header">
+        <h1>Lambda Eats</h1>
+        <div class="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+        </div>
+      </header>
     </>
   );
 };
