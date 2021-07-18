566bd036167d4214b5f9cc66f298d9befc0c9338
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-18 03:54:48 -0400

    Styled home component better

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,20 @@ import styled from "styled-components";
 import DeliveryExamples from "./DeliveryExamples";
 
 const StyledHome = styled.div`
+  h2 {
+    text-align: center;
+    font-size: 3.5rem;
+    color: white;
+    text-shadow: 2px 2px black;
+  }
+
+  button {
+    font-size: 1.15rem;
+    margin: 30%;
+    padding: 20%;
+    border-radius: 10%;
+  }
+
   .main-pizza-div {
     background-image: url("https://www.agrodolce.it/app/uploads/2016/02/pizza-napoletana-980x400.jpg");
     background-repeat: no-repeat;
