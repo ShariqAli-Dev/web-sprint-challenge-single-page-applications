7d38cc4d32e926d956a9bbf5325280cd96c2a8af
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-18 22:58:22 -0400

    Added form validation and state to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, Route, Switch } from "react-router-dom";
+import * as yup from "yup";
 import styled from "styled-components";
+import axios from "axios";
 
+import schema from "./validation/formSchema";
 import Form from "./components/Form";
 import Home from "./components/Home";
 import OrderCompleted from "./components/OrderCompleted";
@@ -63,7 +66,74 @@ const initialFormErrors = {
   sauce: "",
 };
 
+const initialDisabled = true;
+
 const App = () => {
+  const [orders, setOrders] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+  };
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
+  const postOrder = (newOrder) => {
+    axios
+      .post("https://reqres.in/api/orders", newOrder)
+      .then((res) => {
+        setOrders(orders.concat(res.data));
+      })
+      .catch((err) => {
+        debugger;
+      })
+      .finally(setFormValues(initialFormValues));
+  };
+
+  const formSubmit = () => {
+    const newOrder = {
+      special: formValues.special.trim(),
+      name: formValues.name.trim(),
+      size: formValues.size.trim(),
+      sauce: formValues.sauce.trim(),
+      pepperoni: formValues.pepperoni,
+      sausage: formValues.sausage,
+      canadian: formValues.canadian,
+      green: formValues.green,
+      pineapple: formValues.pineapple,
+      extra: formValues.extra,
+    };
+    postOrder(newOrder);
+  };
+
   return (
     <StyledApp>
       <header class="store-header">
@@ -84,7 +154,7 @@ const App = () => {
         </Route>
 
         <Route path="/pizza">
-          <Form />
+          <Form values={formValues} change={inputChange} submit={formSubmit} disabled={disabled} erros={formErrors} />
         </Route>
 
         <Route path="/">
